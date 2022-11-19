881729207e822c09bfdd9d17b13a8980b725c850
Kervin Espirito Santo <57775586+kervines@users.noreply.github.com>
2022-11-18 22:51:27 -0300

    Adicionada a pasta com todos os arquivos -pokedex

---
+++ js/script.js
@@ -0,0 +1,61 @@
+const pokemonName = document.querySelector('.pokemon-name');
+const pokemonNumber = document.querySelector('.pokemon-number');
+const pokemonImage = document.querySelector('.pokemon-image');
+
+const form = document.querySelector('.form');
+const input = document.querySelector('.search-pokemon');
+const buttonPrev = document.querySelector('.btn-prev');
+const buttonNext = document.querySelector('.btn-next');
+
+let searchPokemon = 1;
+
+/*função assincrona definida com async, e as constantes esperam resposta com await*/
+const fetchPokemon = async (pokemon) => {
+  const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+  
+  if(APIResponse.status === 200){
+    const data = await APIResponse.json();
+    return data;
+  }
+};
+
+const renderPokemon = async (pokemon) => {
+  pokemonName.innerHTML = 'Loading...';
+  pokemonNumber.innerHTML = '';
+
+  const data = await fetchPokemon(pokemon);
+  
+  if(data) {
+    pokemonImage.style.display = 'block';
+
+    pokemonName.innerHTML = data.name;
+    pokemonNumber.innerHTML = data.id;
+    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+    input.value='';
+    searchPokemon = data.id;
+  } else {
+    input.value='';
+    pokemonImage.style.display = 'none';
+    pokemonName.innerHTML = 'Not found ;(';
+    pokemonNumber.innerHTML = '';
+  }
+};
+
+form.addEventListener('submit', (event)=>{
+  event.preventDefault();
+  renderPokemon(input.value.toLowerCase());
+});
+
+buttonPrev.addEventListener('click', ()=>{
+  if (searchPokemon > 1){
+    searchPokemon = searchPokemon - 1;
+    renderPokemon(searchPokemon);
+  } 
+});
+
+buttonNext.addEventListener('click', ()=>{
+  searchPokemon = searchPokemon + 1;
+  renderPokemon(searchPokemon);
+});
+
+renderPokemon(searchPokemon);
